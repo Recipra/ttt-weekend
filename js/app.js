9ff479565a536c5080a13f7ddfee47f24344fe99
recipra <jmo0425@outlook.com>
2022-06-01 19:33:11 -0400

    correct render function, remove styleSquare, and add message render to render function

--- js/app.js
+++ js/app.js
@@ -17,23 +17,31 @@ const messageEl = document.getElementById('message')
 
 /*-------------------------------- Functions --------------------------------*/
 init()
-render()
+// render()
 
 function init() {
-  board = [null, null, null, null, null, null, null, null, null]
+  board = [1, null, -1, null, null, 1, null, null, null]
   turn = 1
   winner = null
-  // render()
+  render()
 }
+// turn = (turn === 1) ? -1 : 1
+// console.log(turn)
 
 function render() {
   board.forEach((square, index) => {
-    square = squareEls[index]
-    styleSquare(square)
+    if (square === 1) squareEls[index].textContent = 'X'
+    else if (square === -1) squareEls[index].textContent = 'O'
   });
-  turn = (turn === 1) ? -1 : 1
-}
-
-function styleSquare(square) {
-  console.log(`styling for ${square} goes here`)
+  if (winner === null) {
+    if (turn === 1) {
+      messageEl.textContent = "It is X's turn"
+    } else if (turn === -1) {
+      messageEl.textContent = "It is O's turn"
+    }
+  } else if (winner === 'T') {
+    messageEl.textContent = "It's a tie!"
+  } else {
+    messageEl.textContent = `Congratulations ${winner}'s, you win!`
+  }
 }
