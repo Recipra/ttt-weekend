787982ba734bdb0721c81cc57a625effdee730dd
recipra <jmo0425@outlook.com>
2022-06-01 16:30:20 -0400

    add init, render, and styleSquare functions

--- js/app.js
+++ js/app.js
@@ -7,9 +7,8 @@ let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-const squareEls = document.querySelectorAll('div')
+const squareEls = document.querySelectorAll('.board-squares')
 const messageEl = document.getElementById('message')
-
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -17,4 +16,24 @@ const messageEl = document.getElementById('message')
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+render()
 
+function init() {
+  board = [null, null, null, null, null, null, null, null, null]
+  turn = 1
+  winner = null
+  // render()
+}
+
+function render() {
+  board.forEach((square, index) => {
+    square = squareEls[index]
+    styleSquare(square)
+  });
+  turn = (turn === 1) ? -1 : 1
+}
+
+function styleSquare(square) {
+  console.log(`styling for ${square} goes here`)
+}
